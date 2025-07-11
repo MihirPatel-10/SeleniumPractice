9cbc590ff0c78b00d0217be5c8b3e83d93870c4f
MihirPatel-10 <mihir246803@gmail.com>
2025-07-11 15:56:06 +0530

    Selenium Project

--- src/main/java/SeleniumIntro/Locators.java
+++ src/main/java/SeleniumIntro/Locators.java
@@ -4,13 +4,21 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.time.Duration;
+
 public class Locators {
     public static void main(String[] args) {
+        //implicit wait - 5 second time out
+
+
         System.setProperty("webdriver.chrome.driver", "/home/mihirpatel/Documents/chromedriver");
         WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
         driver.get("https://rahulshettyacademy.com/locatorspractice/");
         driver.findElement(By.id("inputUsername")).sendKeys("Mihir");
         driver.findElement(By.name("inputPassword")).sendKeys("Hello123");
         driver.findElement(By.className("signInBtn")).click();
+        System.out.println(driver.findElement(By.cssSelector("p.error")).getText());
+
     }
 }
