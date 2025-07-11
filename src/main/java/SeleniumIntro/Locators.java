12ec246ebd5b0093533df026944fec22dd180232
MihirPatel-10 <mihir246803@gmail.com>
2025-07-11 17:51:15 +0530

    Selenium Project

--- src/main/java/SeleniumIntro/Locators.java
+++ src/main/java/SeleniumIntro/Locators.java
@@ -19,6 +19,7 @@ public class Locators {
         driver.findElement(By.name("inputPassword")).sendKeys("Hello123");
         driver.findElement(By.className("signInBtn")).click();
         System.out.println(driver.findElement(By.cssSelector("p.error")).getText());
+        driver.findElement(By.linkText("Forgot your password?")).click();
 
     }
 }
